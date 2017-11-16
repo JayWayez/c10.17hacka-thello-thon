b16f3c3e0a4b20f4aa9df8ec72445169358733a5
jay <jamesjayko@gmail.com>
2017-11-16 10:14:33 -0800

    board css

--- main.js
+++ main.js
@@ -38,9 +38,11 @@ function othello(){
 
                 var cellDomElement = cell.createDomElement( this.handleBlockClick.bind(this) );
                 if(y%2===0 && x%2===0){
-                    cell.domElement[0].style.backgroundColor="red";
+                    cell.domElement[0].style.backgroundImage="url('images/bg_2.png')";
                 }else if(y%2!==0 && x%2!==0){
-                    cell.domElement[0].style.backgroundColor="red";
+                    cell.domElement[0].style.backgroundImage="url('images/bg_2.png')";
+                } else {
+                    cell.domElement[0].style.backgroundImage="url('images/bg_1.png')";
                 }
                 row.push(cell);
                 this.containerElement.append(cellDomElement);
@@ -116,7 +118,7 @@ function houseList (){
 
     var player1 = {
         'house' : 'stark',
-        'coinImage': 'images/stark.jpeg',
+        'coinImage': 'images/stark.png',
         'audio': 'audio/...',
         'flagImage': 'image/flag/...',
         'backgroundImg': 'image/background/...',
@@ -125,7 +127,7 @@ function houseList (){
     }
     var player2 = {
         'house' : 'greyjoy',
-        'coinImage': 'images/greyjoy.jpeg',
+        'coinImage': 'images/greyjoy.png',
         'audio': 'audio/...',
         'flagImage': 'image/flag/...',
         'backgroundImg': 'image/background/...',
